3487a9111f2d17e8ba07679905138cdc88c9b0df
Seth Lowie <slowie@powerchordsystem.com>
2019-03-26 21:55:27 -0400

    get people

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,83 @@
-module Main exposing(..)
+module Main exposing (Msg(..), init, main, subscriptions, update, view)
 
-import Html exposing (text)
+import Browser exposing (UrlRequest(..))
+import Browser.Navigation exposing (Key)
+import Html.Styled exposing (..)
+import Http
+import Json.Decode as Decode exposing (..)
+import Url exposing (Url)
+
+
+init : String -> Url -> Key -> ( List Person, Cmd Msg )
+init flags url key =
+    ( [], getPeople )
+
+
+view model =
+    { title = "fdsa"
+    , body =
+        [ toUnstyled <|
+            div []
+                (List.map
+                    (\p ->
+                        div []
+                            [ text
+                                p.name
+                            ]
+                    )
+                    model
+                )
+        ]
+    }
+
+
+update msg model =
+    case msg of
+        GetPeople (Ok res) ->
+            ( res, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+type alias Person =
+    { name : String }
+
+
+personDecoder : Decoder Person
+personDecoder =
+    Decode.map Person
+        (field "name" string)
+
+
+peopleDecoder : Decoder (List Person)
+peopleDecoder =
+    field "results" (list personDecoder)
+
+
+type Msg
+    = ChangedUrl Url
+    | ClickedLink UrlRequest
+    | GetPeople (Result Http.Error (List Person))
+
+
+getPeople =
+    Http.get
+        { url = "https://swapi.co/api/people/"
+        , expect = Http.expectJson GetPeople peopleDecoder
+        }
+
+
+subscriptions model =
+    Sub.none
+
 
 main =
-   text "SWAPI!!!"
+    Browser.application
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        , onUrlChange = ChangedUrl
+        , onUrlRequest = ClickedLink
+        }
